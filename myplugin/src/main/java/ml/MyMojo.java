1918821cc1255fed39b79667bf29a9b38f85e609
xmarlem <inglembomarco@gmail.com>
2018-12-02 20:00:33 +0100

    prep for release

--- myplugin/src/main/java/ml/MyMojo.java
+++ myplugin/src/main/java/ml/MyMojo.java
@@ -18,18 +18,13 @@ package ml;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Goal which touches a timestamp file.
- *
- * @goal touch
- * 
- * @phase process-sources
- */
+@Mojo(name="touch")
 public class MyMojo
     extends AbstractMojo
 {
