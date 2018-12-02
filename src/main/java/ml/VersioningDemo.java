97dbef7690898466041d48c4e6dbff255064bc5a
xmarlem <inglembomarco@gmail.com>
2018-12-02 14:57:13 +0100

    first commit

---
+++ src/main/java/ml/VersioningDemo.java
@@ -0,0 +1,10 @@
+package ml;
+
+public class VersioningDemo {
+    public static void main(String args[]){
+        System.out.println("Hello explorers.");
+        System.out.println();
+        System.out.println("This program is running version " +
+                VersioningDemo.class.getPackage().getImplementationVersion());
+    }
+}
